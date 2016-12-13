36737039c900a1c1fdc7dd84a63b926bac34f22b
Nejat <nejatoz@gmail.com>
2016-12-12 23:08:18 -0500

    cleaned up dead code

--- src/test/kotlin/org/nexivo/kt/specifics/test/utilities.testing.kt
+++ src/test/kotlin/org/nexivo/kt/specifics/test/utilities.testing.kt
@@ -20,33 +20,3 @@ inline fun <reified T> throws(block: () -> Unit)
 
     throw AssertionError("Expected ${className<T>()} was NOT Thrown!")
 }
-
-fun String.mockedStream (): InputStream = mockedStream(*this.map(Char::toInt).toIntArray())
-
-fun mockedStream (vararg values: Int): InputStream =
-
-    object : MockUp<InputStream>() {
-
-        val iterated: Iterator<Int> = values.iterator()
-        var complete: Boolean       = false
-
-        @Mock fun read(): Int = if (iterated.hasNext()) { iterated.next() } else { -1 }
-
-        @Mock fun read(b: ByteArray, off: Int, len: Int): Int =
-            if (complete) {
-                -1
-            } else {
-
-                val end = off + len
-
-                (off..end).forEachIndexed { idx, value ->
-                    if(value < values.size) {
-                        b[idx] = values[value].toByte()
-                    }
-                }
-
-                complete = end >= values.size
-
-                values.size
-            }
-    }.mockInstance
